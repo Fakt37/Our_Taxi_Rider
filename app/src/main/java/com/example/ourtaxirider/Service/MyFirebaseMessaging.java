e327283f8351c2e28613c643a98f9a250fb0a2a2
Fakt37 <fakt37@gmail.com>
2020-06-08 20:36:46 +0300

    cancel request

--- app/src/main/java/com/example/ourtaxirider/Service/MyFirebaseMessaging.java
+++ app/src/main/java/com/example/ourtaxirider/Service/MyFirebaseMessaging.java
@@ -1,6 +1,9 @@
 package com.example.ourtaxirider.Service;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 
 import com.example.ourtaxirider.Common.Common;
 import com.example.ourtaxirider.Model.Token;
@@ -23,8 +26,14 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
     }
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-
+    public void onMessageReceived(final RemoteMessage remoteMessage) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MyFirebaseMessaging.this, ""+remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void updateTokenToServer(String refreshedToken) {
