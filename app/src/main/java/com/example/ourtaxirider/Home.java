2c1757e3f90bc70a61967eb7091a6f254ec05777
Fakt37 <fakt37@gmail.com>
2020-06-08 12:38:48 +0300

    first code from driver app

---
+++ app/src/main/java/com/example/ourtaxirider/Home.java
@@ -0,0 +1,74 @@
+package com.example.ourtaxirider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class Home extends AppCompatActivity implements OnMapReadyCallback {
+    SupportMapFragment mapFragment;
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+        .position(new LatLng(37.7750, -122.4183))
+        .title("Это Вы"));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.7750, -122.4183), 15));
+    }
+}
--- app/src/main/java/com/example/ourtaxirider/MainActivity.java
+++ app/src/main/java/com/example/ourtaxirider/MainActivity.java
@@ -1,14 +1,247 @@
 package com.example.ourtaxirider;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.example.ourtaxirider.Model.Rider;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import dmax.dialog.SpotsDialog;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSignIn, btnRegister;
+    RelativeLayout rootLayout;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+
+    private final static int PERMISSION = 1000;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/ValueSans.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
         setContentView(R.layout.activity_main);
+        //Init Firebase
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Riders");
+        //Init View
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        rootLayout = (RelativeLayout)findViewById(R.id.rootLayout);
+        //Event
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterDialog();
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLoginDialog();
+            }
+        });
+    }
+
+    private void showLoginDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("АВТОРИЗАЦИЯ ");
+        dialog.setMessage("Введите данные для входа");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View login_layout = inflater.inflate(R.layout.layout_login, null);
+
+        final MaterialEditText edtEmail = login_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = login_layout.findViewById(R.id.edtPassword);
+
+        dialog.setView(login_layout);
+
+        //Set button
+        dialog.setPositiveButton("ВХОД", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                //Set disable button Sign In if is processing
+                btnSignIn.setEnabled(false);
+                //Check validation
+                if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+                    Snackbar.make(rootLayout, "Введите Email", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, "Введите пароль", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (edtPassword.getText().toString().length() < 6) {
+                    Snackbar.make(rootLayout, "Пароль слишком короткий", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                final AlertDialog waitingDialog= new
+                        SpotsDialog.Builder().setContext(MainActivity.this).build();
+                waitingDialog.setMessage("Загрузка .....");
+                waitingDialog.show();
+                //Login
+                auth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                waitingDialog.dismiss();
+                                startActivity(new Intent(MainActivity.this, Home.class));
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                waitingDialog.dismiss();
+                                Snackbar.make(rootLayout, "Ошибка "+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                        .show();
+                                //Active button
+                                btnSignIn.setEnabled(true);
+                            }
+                        });
+
+            }
+        });
+        dialog.setNegativeButton("ОТМЕНА", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private void showRegisterDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("РЕГИСТРАЦИЯ ");
+        dialog.setMessage("Введите данные для регистрации");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_layout = inflater.inflate(R.layout.layout_register, null);
+
+        final MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
+        final MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
+        final MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
+
+        dialog.setView(register_layout);
+
+        //Set button
+        dialog.setPositiveButton("РЕГИСТРАЦИЯ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                //Check validation
+                if (TextUtils.isEmpty(edtEmail.getText().toString()))
+                {
+                    Snackbar.make(rootLayout, "Введите Email", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPassword.getText().toString()))
+                {
+                    Snackbar.make(rootLayout, "Введите пароль", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (edtPassword.getText().toString().length() < 6)
+                {
+                    Snackbar.make(rootLayout, "Пароль слишком короткий", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtName.getText().toString()))
+                {
+                    Snackbar.make(rootLayout, "Введите имя", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPhone.getText().toString()))
+                {
+                    Snackbar.make(rootLayout, "Введите номер телефона", Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                //Register new user
+                auth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                //Save user to db
+                                Rider rider = new Rider();
+                                rider.setEmail(edtEmail.getText().toString());
+                                rider.setName(edtName.getText().toString());
+                                rider.setPhone(edtPhone.getText().toString());
+                                rider.setPassword(edtPassword.getText().toString());
+
+                                //Use email to key
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(rider)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(rootLayout, "Регистрация прошла успешно!", Snackbar.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Snackbar.make(rootLayout, "Ошибка регистрации!"+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        });
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout, "Ошибка"+e.getMessage(), Snackbar.LENGTH_SHORT)
+                                        .show();
+                            }
+                        });
+            }
+        });
+        dialog.setNegativeButton("ОТМЕНА", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
